bdae0f428cad25c8b166bea3a613a5fd08ba768a
J2mF <mp4-12cs5@hotmail.com>
2020-07-29 18:52:01 +0300

    Implemented Enemy Movement Closes #2

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+    [SerializeField][Tooltip("List of waypoints that define an enemy path with thier positions")] private List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    private int wayPointIndex = 0;
+
+    public int WayPointIndex { get => wayPointIndex; }
+
+    public int IncrementWayPointIndex() => ++wayPointIndex;
+
+    public void Start() {
+        InitializePositionToFirstWaypoint();
+    }
+
+    private void InitializePositionToFirstWaypoint() {
+        transform.position = waypoints[0].position;
+    }
+
+    private void Update() {
+        if ( LastWayPointReached() ) {
+            Destroy( gameObject );
+            return;
+        }
+
+        Move();
+    }
+
+    private void Move() {
+        Vector3 targetPosition = waypoints[wayPointIndex].position;
+        float moveSpeedFrameIndependant = moveSpeed * Time.deltaTime;
+
+        Vector3 newPosition = Vector2.MoveTowards( transform.position, targetPosition, moveSpeedFrameIndependant );
+        transform.position = newPosition;
+
+        if ( transform.position == targetPosition )
+            IncrementWayPointIndex();
+    }
+
+    private bool LastWayPointReached() {
+        return wayPointIndex >= waypoints.Count;
+    }
+}
