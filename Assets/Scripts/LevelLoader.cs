e15f5e60faa4287225837d6ef8bfeeafa0f1e6c7
J2mF <mp4-12cs5@hotmail.com>
2020-11-20 10:21:16 +0200

    Add Main Menu and Game Over Scenes

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    private void Awake() {
+        DontDestroyOnLoad( gameObject );
+    }
+
+    public void LoadGameOverScene() {
+        SceneManager.LoadScene( "Game Over" );
+    }    
+    
+    public void LoadStartMenuScene() {
+        SceneManager.LoadScene( 0 );
+    }
+
+    public void LoadGameScene() {
+        SceneManager.LoadScene( "Game" );
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
